b56d1a75f47ddbc1fc8a0991431de04a21e3e2c8
your name <nhatminh14903@gmail.com>
2023-03-14 09:55:09 +0700

    baith14/3con5bai

--- javacode/BaiThucHanhLab2/Bai1.java
+++ javacode/BaiThucHanhLab2/Bai1.java
@@ -1,44 +1,22 @@
 import java.util.Scanner;
 
 public class Bai1 {
-    public static void main(String[] args) throws Exception {
+
+    public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+        System.out.print("nhap a: ");
+        float a = sc.nextFloat();
+        System.out.print("nhap b: ");
+        float b = sc.nextFloat();
+        // 1
+        System.out.println("tong = " + (a + b));
+        System.out.println("tru = " + (a - b));
+        System.out.println("nhan = " + (a * b));
+        System.out.println("chia = " + (a / b));
 
-        int a = 0, b = 0;
-        int sl;
-        System.out.print("Nhap A= ");
-        a = sc.nextInt();
-        System.out.print("Nhap B= ");
-        b = sc.nextInt();
-        System.out.println("Nhap vao lua chon cua ban: ");
-        System.out.println("1-Tinh tong ");
-        System.out.println("2-Tinh hieu ");
-        System.out.println("3-Tinh tich ");
-        System.out.println("4-Tinh thuong ");
-        sl = sc.nextInt();
-        switch (sl) {
-            case 1: {
-                int tong = a + b;
-                System.out.print(a + " + " + b + " = " + tong);
-                break;
-            }
-            case 2: {
-                int hieu = a - b;
-                System.out.print(a + " - " + b + " = " + hieu);
-                break;
-            }
-            case 3: {
-                int tich = a * b;
-                System.out.print(a + " * " + b + " = " + tich);
-                break;
-            }
-            case 4: {
-                int thuong = a / b;
-                float l = thuong;
-                System.out.print(a + " / " + b + " = " + l + thuong);
-                break;
-            }
-
-        }
+        // 2
+        System.out.println((a > b) ? "a>b" : "a<b");
     }
+
 }
