f854883b2b92563b03daadeaabfee78df07d9e9f
your name <nhatminh14903@gmail.com>
2023-04-25 00:44:26 +0700

    bth2-18/4

---
+++ BT.LAP3/src/DANHBA/function.java
@@ -0,0 +1,13 @@
+package DANHBA;
+
+import java.util.Scanner;
+
+public interface function {
+    public abstract void NhapThongTin(Scanner sc);
+
+    public abstract void Update(Scanner sc);
+
+    public abstract void Delete(Scanner sc);
+
+    public abstract void Search(Scanner sc);
+}
